e9be842a44c6508c0e352fb67fd464646253f491
klfischer89 <fischer-k4@web.de>
2025-11-25 11:14:48 +0100

    change visibility to protected

--- com/cc/java/SuperCat.java
+++ com/cc/java/SuperCat.java
@@ -12,7 +12,7 @@ public class SuperCat {
     this.age = age;
   } 
   
-  public String getStringAttributes(String op) {
+  protected String getStringAttributes(String op) {
     switch (op) {
       case "#name":
         return firstName;
